ba105527533c28715f04c0b07f9358e703a5fad3
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2023-10-18 18:33:25 +0400

    ok

--- Sem_3/script.js
+++ Sem_3/script.js
@@ -1,85 +1,68 @@
 "use strict";
 
-// Задание 1: "Управление персоналом компании"
+// Задание 1: "Управление библиотекой книг"
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+// Пример использования класса
 
-// Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
-// Свойство name (имя) - строка, имя сотрудника.
-// Метод displayInfo() - выводит информацию о сотруднике (имя) в консоль.
-
-// Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
-// Свойство department (отдел) - строка, отдел, в котором работает менеджер.
-// Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
-
-class Employee {
-  constructor(name) {
-    this.name = name;
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
   }
   displayInfo() {
-    console.log(`Name: ${this.name}`);
-  }
-}
-
-class Manager extends Employee {
-  constructor(name, department) {
-    super(name);
-    this.department = department;
-    this.displayInfo =  () => {
-      console.log(`Name: ${this.name}, Deparment: ${this.department}`);
-    }
-  }
-}
-
-const employee = new Employee("John Smith");
-employee.displayInfo(); // "Name: John Smith"
-
-const manager = new Manager("Jane Doe", "Sales");
-manager.displayInfo(); // "Name: John Doe, Department: Sales"
-
-
-// Задание 2: "Управление списком заказов"
-
-// Реализуйте класс Product (товар), который имеет следующие свойства и методы:
-// Свойство name - название товара.
-// Свойство price - цена товара.
-// Свойство quantity - количество товара.
-
-// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
-// Свойство id (номер заказа) - число, уникальный номер заказа.
-// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
-// Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
-// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
-
-class Product {
-  constructor(name, price, quantity) {
-    this.name = name;
-    this.price = price;
-    this.quantity = quantity;
-  }
-}
-
-class Order {
-  constructor(id, products = []) {
-    this.id = id;
-    this.products = products;
-  }
-  addProduct(product) {
-    this.products.push(product);
-  }
-  getTotalPrice() {
-    return this.products.reduce(
-      (sum, item) => (sum += item.price * item.quantity),
-      0
+    console.log(
+      `Tittle: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`
     );
   }
 }
 
-// Пример использования:
-const order = new Order(12345);
+const book = new Book("BookName1", "Author1", 123);
+book.displayInfo(); // "Title: BookName1, Author: Author1, Pages: 123"
 
-const product1 = new Product("Phone", 500, 2);
-order.addProduct(product1);
+// Задание 2: "Управление списком студентов"
+// Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
 
-const product2 = new Product("Headphones", 100, 1);
-order.addProduct(product2);
+// Свойство name (имя) - строка, имя студента.
+// Свойство age (возраст) - число, возраст студента.
+// Свойство grade (класс) - строка, класс, в котором учится студент.
+// Метод displayInfo() - выводит информацию о студенте в консоль.
 
-console.log(order.getTotalPrice()); // Вывод: 1100
+// Пример использования класса
+
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+  }
+}
+
+const student = new Student("John Smith", 16, "10th grade");
+student.displayInfo(); // "Name: John Smith, Age: 16, Grade: 10th grade"
+
+// К задаче 2-ого семинара где банкомат у нас принимает деньги, с точностью не более копеек ,как я и обещал, написал простую проверку без перевода в строки и подсчета длин "хвостов":
+//  возможно прверять 15 цифр после запятой (например 11.000000000000001), если вводится длиннее, то на программу это уже и не влияет.
+
+function checkAccount(account) {
+  const accountX100 = account * 100;
+  accountX100 - Math.trunc(accountX100) > 0
+    ? console.log("Число содержит знаки после сотых")
+    : console.log("число корректное!");
+}
+
+// примеры:
+checkAccount(11); // "число корректное!"
+checkAccount(11.1); // "число корректное!"
+checkAccount(11.01); // "число корректное!"
+
+checkAccount(11.001);              //"Число содержит цифры после сотых"
+checkAccount(11.000000000000001);  //"Число содержит цифры после сотых"
+checkAccount(11.000007000000001);  //"Число содержит цифры после сотых"
